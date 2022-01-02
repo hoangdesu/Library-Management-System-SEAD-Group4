11c315c46aa00d76bd09deb088e3d161aa14d351
hoangdesu <hoangdesu@gmail.com>
2022-01-02 18:40:55 +0700

    Fix build script, cleanup client code

--- client/src/pages/dashboard/dashboard.js
+++ client/src/pages/dashboard/dashboard.js
@@ -1,46 +1,49 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Container } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
-import {fetchUserData} from '../../api/authenticationService';
+import { fetchUserData } from '../../api/authenticationService';
 
-
-const MainWrapper=styled.div`
-    padding-top:40px;
+const MainWrapper = styled.div`
+    padding-top: 40px;
 `;
 
-export const Dashboard=(props)=>{
+export const Dashboard = (props) => {
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({});
 
-    const dispatch=useDispatch();
-    const [loading,setLoading]=useState(false);
-    const [data,setData]=useState({});
+    useEffect(() => {
+        fetchUserData()
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch((e) => {
+                localStorage.clear();
+                props.history.push('/');
+            });
+    }, [props.history]);
 
-    React.useEffect(()=>{
-        fetchUserData().then((response)=>{
-            setData(response.data);
-        }).catch((e)=>{
-            localStorage.clear();
-            props.history.push('/');
-        })
-    },[])
-
-    const logOut=()=>{
-
+    const logOut = () => {
         localStorage.clear();
         props.history.push('/');
-
-    }
+    };
 
     return (
         <Container>
             <MainWrapper>
                 <h4>Hello {data && `${data.firstName} ${data.lastName}`}</h4>
                 <br></br>
-                {data && data.roles && data.roles.filter(value => value.roleCode==='ADMIN').length>0 && <Button type="variant">Add User</Button> }
+                {data &&
+                    data.roles &&
+                    data.roles.filter((value) => value.roleCode === 'ADMIN')
+                        .length > 0 && <Button type="variant">Add User</Button>}
                 <br></br>
 
-                <Button style={{marginTop:'5px'}} onClick={() =>logOut()}>Logout</Button>
+                <Button style={{ marginTop: '5px' }} onClick={() => logOut()}>
+                    Logout
+                </Button>
             </MainWrapper>
         </Container>
-    )
-}
+    );
+};
