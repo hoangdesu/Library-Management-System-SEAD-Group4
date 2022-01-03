927c429ef66d8b7ff453390e0991f16d3cb92d55
ducnt12 <hello.there.two.nine@gmail.com>
2022-01-03 13:03:30 +0700

    new

--- client/src/pages/homepage/homepage.jsx
+++ client/src/pages/homepage/homepage.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Slider from "react-slick";
 
 import "../../styles/homepage.css"
 
 export default function Homepage() {
+
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3500
+    };
+
     return (
         <div>
             <div className="jumbotron1">
@@ -20,29 +32,62 @@ export default function Homepage() {
 
             </div>
 
-            <div className="jumbotron2 card bg-dark text-white">
-
+            <div className="jumbotron2 card bg-dark text-white mt-5">
                 <img className="card-img" src="library.png" alt="Card image" />
                 <div className="card-img-overlay">
                     <div >
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title display-6">Card title</h5>
+                        <p className="card-text lead">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <p className="card-text">Last updated 3 mins ago</p>
                     </div>
                 </div>
 
             </div>
 
-            <div className="div1">Another div</div>
+            <div className="div1 mt-5">
+                <h2>Library services and help</h2><br />
+                <Slider {...settings} >
+                    <div className="d-flex justify-content-center align-items-center">
+                        <h3>1</h3>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <h3>2</h3>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <h3>3</h3>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <h3>4</h3>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <h3>5</h3>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <h3>6</h3>
+                    </div>
+                </Slider>
+            </div>
 
-            <div class="carousel-item">
-                <img src="..." alt="..." />
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>...</h5>
-                    <p>...</p>
+            <div className="card mb-3 div1 mt-5" >
+                <div className="row g-0">
+                    <div className="col-md-6 d-flex align-items-center">
+                        <div className="card-body">
+                            <h5 className="card-title display-6">About Us</h5>
+                            <p className="card-text lead">Meet the library team, find out about our hours and locations and read our rules here. </p>
+                            <p className="card-text"><a href=""><small className="text-muted">Discover more about the library</small></a></p>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <img src="https://www.latrobe.edu.au/galleries/sydney-campus-library/library-sydney02.jpg" className="img-fluid rounded-start" alt="..." />
+                    </div>
+
                 </div>
             </div>
-        </div>
+
+            <div className="jumbotron1">
+                Contact Us
+            </div>
+        </div >
 
     )
 }
