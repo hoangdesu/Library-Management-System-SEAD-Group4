639c2e05c2eaa579c3f3d3ddbebaba1fe118a9b4
ducnt12 <hello.there.two.nine@gmail.com>
2022-01-07 19:58:21 +0700

    new

--- client/src/pages/layouts/footer.jsx
+++ client/src/pages/layouts/footer.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import 'font-awesome/css/font-awesome.min.css';
+import "../../styles/layout.css"
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+
+        <footer class="text-center text-white">
+            <div class="container pt-4 pb-0">
+                <section class="mb-4">
+                    <a class="btn btn-outline-light btn-floating m-1 icon" href="" role="button">
+                        <i class="fa fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-floating m-1 icon" href="" role="button">
+                        <i class="fa fa-twitter"></i></a>
+
+                    <a class="btn btn-outline-light btn-floating m-1 icon" href="" role="button">
+                        <i class="fa fa-google"></i></a>
+
+                    <a class="btn btn-outline-light btn-floating m-1 icon" href="" role="button">
+                        <i class="fa fa-instagram"></i></a>
+
+                    <a class="btn btn-outline-light btn-floating m-1 icon" href="" role="button">
+                        <i class="fa fa-linkedin"></i></a>
+
+                    <a class="btn btn-outline-light btn-floating m-1 icon" href="" role="button">
+                        <i class="fa fa-github"></i></a>
+                </section>
+
+            </div>
+
+            <div class="text-center pb-3" >
+                © 2020 Copyright:
+                <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            </div>
+        </footer>
     )
 }
--- client/src/pages/user/UserProfile.jsx
+++ client/src/pages/user/UserProfile.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "../../styles/user-profile.css"
+import 'font-awesome/css/font-awesome.min.css';
 
 export default function UserProfile() {
+    const user = {
+        first_name: "duc",
+        last_name: "nguyen trung"
+    }
+
     return (
-        <div>user profile</div>
+        <div className="body d-flex align-items-start">
+            <div className="tab nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <div className="App">
+                    <img className="avatar rounded-circle border" src="https://icons-for-free.com/iconfiles/png/512/human+person+user+icon-1320196276306824343.png" alt="avatar unavailable" />
+                    <h5>{user.first_name + " " + user.last_name}</h5>
+                    <hr className="my-3" />
+                </div>
+                <button className="nav-link active" id="acc-settings-tab" data-bs-toggle="pill" data-bs-target="#acc-settings" type="button" role="tab" aria-controls="acc-settings" aria-selected="true">Account Settings</button>
+                <button className="nav-link" id="privacy-tab" data-bs-toggle="pill" data-bs-target="#privacy" type="button" role="tab" aria-controls="privacy" aria-selected="false">Privacy</button>
+                <button className="nav-link" id="notifycation-tab" data-bs-toggle="pill" data-bs-target="#notifycation" type="button" role="tab" aria-controls="notifycation" aria-selected="false">Notification</button>
+            </div>
+
+            <div className="tab-content" id="v-pills-tabContent">
+                <div className="tab-pane fade show active" id="acc-settings" role="tabpanel" aria-labelledby="acc-settings-tab">
+                    Account Settings
+                </div>
+                <div className="tab-pane fade" id="privacy" role="tabpanel" aria-labelledby="privacy-tab">...1</div>
+                <div className="tab-pane fade" id="notifycation" role="tabpanel" aria-labelledby="notifycation-tab">...2</div>
+            </div>
+        </div>
     )
 }
