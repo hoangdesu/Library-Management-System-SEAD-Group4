612252f103fe8bba5f384bd33c760d50b3243cbc
ducnt12 <hello.there.two.nine@gmail.com>
2022-01-10 21:39:27 +0700

    new

--- client/src/pages/layouts/navbar.jsx
+++ client/src/pages/layouts/navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "../../styles/layout.css"
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <nav class="nav navbar">
+            <div class="container-fluid">
+                <a class="navbar-brand">Navbar</a>
+                <form class="d-flex input-group w-auto">
+                    <input
+                        type="search"
+                        class="form-control rounded"
+                        placeholder="Search"
+                        aria-label="Search"
+                        aria-describedby="search-addon"
+                    />
+                    <span class="input-group-text border-0" id="search-addon">
+                        <i class="fas fa-search"></i>
+                    </span>
+                </form>
+            </div>
+        </nav>
     )
 }
