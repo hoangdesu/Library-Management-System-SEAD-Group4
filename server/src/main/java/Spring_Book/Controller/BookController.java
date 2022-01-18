dfde2b72f7bd3d5fa0f53e22c616ccee71f0cd5f
hoangdesu <hoangdesu@gmail.com>
2022-01-18 16:27:48 +0700

    minor fix

--- server/src/main/java/Spring_Book/Controller/BookController.java
+++ server/src/main/java/Spring_Book/Controller/BookController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/book")
 public class BookController {
 
